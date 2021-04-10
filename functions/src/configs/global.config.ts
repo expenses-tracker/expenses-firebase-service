71f557a0a310c2184614fa5d81b5ce24f852b835
Varghese Abraham <varnit2010@gmail.com>
2021-04-10 08:57:18 +0530

    Manage mongourl as secret

--- functions/src/configs/global.config.ts
+++ functions/src/configs/global.config.ts
@@ -1,11 +1,7 @@
 import { loggerService } from "core-api-lib";
 
 export const GLOBAL_CONFIG = {
-    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS ? Number(process.env.BCRYPT_SALT_ROUNDS) : 12,
-    database: {
-        produrl: 'mongodb+srv://varnit-mongodb:qD9gPINXmP9C4zXF@azure-asia-cluster.bellz.azure.mongodb.net/expenses?retryWrites=true&w=majority',
-        devurl: 'mongodb+srv://varnit-mongodb:oykq7IiTURHoU03Z@expense-dev.vhxfi.mongodb.net/expenses?retryWrites=true&w=majority'
-    }
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS ? Number(process.env.BCRYPT_SALT_ROUNDS) : 12
 };
 
 export const whiteListedUrls = [
