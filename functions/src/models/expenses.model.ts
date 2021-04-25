a00967411288a3203851ffc8caf1fe1b98b9f6da
Varghese Abraham <varnit2010@gmail.com>
2021-04-25 09:12:26 +0530

    Auto create indexes, bump mongoose version (#6)
    * Auto create indexes
    
    * Bump mongoose version to 5.12.5

--- functions/src/models/expenses.model.ts
+++ functions/src/models/expenses.model.ts
@@ -18,17 +18,19 @@ export interface IExpense {
 type Expense = mongoose.Document & IExpense; 
 
 const ExpenseSchema = new Schema({
-    description: { type: String },
-    dated: { type: Schema.Types.Date, default: Date.now },
+    description: { type: String, index: true },
+    dated: { type: Schema.Types.Date, default: Date.now, index: true },
     amount: { type: Number },
     paymentType: { type: String },
     category: { type: String },
     tag: { type: String },
     subDescription: { type: String },
     createdOn: { type: Schema.Types.Date, default: Date.now },
-    createdBy: { type: String },
+    createdBy: { type: String, index: true },
     updatedOn: { type: Schema.Types.Date, default: Date.now },
     updatedBy: { type: String }
-});
+}, { autoIndex: false });
+ExpenseSchema.index({ dated: 1, category: 1, paymentType: 1 }, { unique: false });
 
 export const ExpenseModel = model<Expense>('expenses', ExpenseSchema);
+ExpenseModel.createIndexes();
