330a791b899659476eef36bf1278ab44e81e1d8d
Varghese Abraham <varnit2010@gmail.com>
2021-04-27 08:52:13 +0530

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -60,11 +60,11 @@ const service_env = ConfigService.allConfigs().environment;
 console.log(`Service Environment ::: ${service_env}`);
 
 const appUrl = (ConfigService.isProdEnv() ? process.env.APP_URL : process.env.DEV_APP_URL) || 'http://localhost:4200';
-const corsOptions = {
-    origin: appUrl,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: appUrl,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+// };
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", appUrl );
   next();
