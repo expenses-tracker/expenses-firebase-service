2e6c7a4b5a2632e1f5644e7749415672e75f1559
Varghese Abraham <varnit2010@gmail.com>
2021-05-06 07:03:35 +0530

    Dashboard charts fixes (#11)

--- functions/src/services/dashboard.service.ts
+++ functions/src/services/dashboard.service.ts
@@ -2,6 +2,7 @@ import { logger, LOG_LEVEL } from "../configs/global.config";
 import { UserMetaData } from "../models/userMetaData.model";
 import { ExpenseService } from "./expenses.service";
 import { IncomeService } from "./incomes.service";
+import * as lodash from 'lodash';
 
 let instance: DashboardService;
 
@@ -22,6 +23,25 @@ export class DashboardService {
         const expenseChart = await ExpenseService.getInstance().findMonthYearWiseExpenses(userData, dateRange);
         const recentIncomes = await IncomeService.getInstance().findLimitedIncomes(userData);
         const incomeChart = await IncomeService.getInstance().findMonthYearWiseIncome(userData, dateRange);
+        const allIdsInExpenseCharts = lodash.sortBy(lodash.map(expenseChart, '_id'), ['month']);
+        const allIdsInIncomeCharts = lodash.sortBy(lodash.map(incomeChart, '_id'), ['month']);
+        const diffInExpenseChart = lodash.differenceBy(allIdsInExpenseCharts, allIdsInIncomeCharts, 'month');
+        const diffInIncomeChart = lodash.differenceBy(allIdsInIncomeCharts, allIdsInExpenseCharts, 'month');
+        diffInExpenseChart.forEach(diff => {
+            incomeChart.push({
+                _id: diff,
+                amount: 0,
+                paymentTypes: []
+            });
+        });
+        diffInIncomeChart.forEach(diff => {
+            expenseChart.push({
+                _id: diff,
+                amount: 0,
+                paymentTypes: [],
+                categories: []
+            });
+        });
         return {
             expenses: {
                 entries: recentExpenses,
